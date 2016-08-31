8ea0a2951edb36ef701ecf63ba0ead5130cd79c2
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2016-08-31 14:44:32 -0300

    Ajuste na área de Pós-Graduação

--- js/app.js
+++ js/app.js
@@ -62,6 +62,7 @@ document.addEventListener("onload", onAppReady, false) ;
 
 $(document).ready(function(){
     $('#protoc_pessoa').change(function(){
+        $('#solicitante').after('<div data-role="fieldcontain" id="documento"></div>');
         $('#documento').empty();
         $("#documento").append('');
         if ( $(this).val() == 0){ // Alunos
