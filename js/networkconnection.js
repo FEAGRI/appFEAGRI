69a75182a37bfe7346e57c33d94b62469873326f
FEAGRI <suporte-intranet@feagri.unicamp.br>
2016-09-05 11:46:26 -0300

    networkiconnect removido

--- js/networkconnection.js
+++ js/networkconnection.js
@@ -34,7 +34,6 @@
             var year = mydate.getFullYear();    
             var idade = (year - ano);
 
-            
             if(usuarioVal !== "0" ){
                 window.plugins.OneSignal.sendTag("usuario", usuario);
                 window.plugins.OneSignal.sendTag("matricula", matricula);
