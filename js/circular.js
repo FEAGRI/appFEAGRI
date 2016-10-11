c5548dfa2dd163faec20b5d59a346ce40e2d0cf7
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2016-10-11 19:05:47 -0300

    Circular Finalizado

--- js/circular.js
+++ js/circular.js
@@ -1,34 +1,43 @@
 $(document).ready(function() {
 
-    $('.btn-circular').click(function() {
+
+    $('a.btn-circular').click(function() {
+        $("#result-circular").empty();
         var baseurl = "http://www.feagri.unicamp.br/portal/sistemas-intranet/circular-interno?";
         var ci_circular = $("#ci_circular").val();
         var ci_ponto = $("#ci_ponto").val();
-
-        //var url = baseurl + "ci_circular=" + ci_circular + "&ci_ponto=" + ci_ponto;
-        var url = "js/mock/circular.json";
-        // busca a URL e cria o array
+        var url = baseurl + "ci_circular=" + ci_circular + "&ci_ponto=" + ci_ponto;
+        //var url = "js/circular.json";
         $.getJSON(url)
             .done(function(data) {
                 var result = '';
+                var pne = '';
                 //limpa a div (se vc modificar ela não duplica resultados)	
-                $("#result").empty();
+                $("#result-circular").empty();
+                $("#result-circular").append('<h4>Circular ' + ci_circular + ' - Ponto: ' + ci_ponto + '</h4>');
                 for (var i = 0; i < data.length; i++) {
+                    if (data[i]["PNE"] === "SIM") {
+                        pne = '<i class="fa fa-wheelchair fa-lg"></i>';
+                    } else { pne = '' }
                     var listHtml = '<li>';
                     listHtml += '<div class="circular-num">' + data[i]["C"] + '</div>';
-                    listHtml += '<div class="circular-via">' + data[i]["Via"] + '</div>';
-                    listHtml += '<div class="circular-pne">' + data[i]["PNE"] + '</div>';
+                    if (data[i]["Via"] !== "") {
+                        listHtml += '<div class="circular-via">' + data[i]["Via"] + '</div>';
+                    }
+                    listHtml += '<div class="circular-pne">' + pne + '</div>';
                     listHtml += '<div class="circular-ponto">' + data[i]["Ponto"] + '</div>';
                     listHtml += '<div class="circular-horario">' + data[i]["Horário"] + '</div>';
                     listHtml += '</li>';
                     result += listHtml;
 
                 }
-                $("#result").append(result);
+
+                $("#result-circular").append(result);
 
             })
             .fail(function() {
-                $("#result").append("Não existem resultados a exibir!");
+                $("#result-circular").empty();
+                $("#result-circular").append('<p class="alerta">Não existem resultados a exibir!</p>');
             });
     });
 
--- js/contatos.js
+++ js/contatos.js
@@ -1,67 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-		//var url = "http://www.feagri.unicamp.br/portal/sistemas-intranet/contatos";
-		var url = 'http://www.feagri.unicamp.br/portal/templates/simplesimon/includes/contatos.json';
-		
-		var $listacontatos = $("#listacontatos");
-		$.getJSON(url)
-		.done(function(data){
+    //var url = "http://www.feagri.unicamp.br/portal/sistemas-intranet/contatos";
+    var url = 'http://www.feagri.unicamp.br/portal/templates/simplesimon/includes/contatos.json';
 
-			$listacontatos.empty();
-			
-			var contatosHTML = '<ul data-role="listview" data-filter="true" data-autodividers="true" data-filter-placeholder="Buscar..." data-inset="true">';           
-            $.each(data, function(i, data){
-            	contatosHTML += '<li>';
-	            contatosHTML += '<a href="#contatoItem';
-	            contatosHTML += i;
-	            contatosHTML += '">';//link;
-				contatosHTML += '<img src="http://www.feagri.unicamp.br/portal/';
-				contatosHTML += data.foto;
-				contatosHTML += '">';
-				contatosHTML += '<h2>';
-				contatosHTML += data.nome;
-				contatosHTML += '</h2>';
-				contatosHTML += '<p>';
-				contatosHTML += data.gruponame +' | '+ data.funcao;
-				contatosHTML += '</p>';
-				contatosHTML += '</a></li>';	 
+    var $listacontatos = $("#listacontatos");
+    $.getJSON(url)
+        .done(function(data) {
+
+            $listacontatos.empty();
+
+            var contatosHTML = '<ul data-role="listview" data-filter="true" data-autodividers="true" data-filter-placeholder="Buscar..." data-inset="true">';
+            $.each(data, function(i, data) {
+                contatosHTML += '<li>';
+                contatosHTML += '<a href="#contatoItem';
+                contatosHTML += i;
+                contatosHTML += '">'; //link;
+                contatosHTML += '<img src="http://www.feagri.unicamp.br/portal/';
+                contatosHTML += data.foto;
+                contatosHTML += '">';
+                contatosHTML += '<h2>';
+                contatosHTML += data.nome;
+                contatosHTML += '</h2>';
+                contatosHTML += '<p>';
+                contatosHTML += data.gruponame + ' | ' + data.funcao;
+                contatosHTML += '</p>';
+                contatosHTML += '</a></li>';
             });
             contatosHTML += '</ul>';
-            
-            $listacontatos.html(contatosHTML);
-			            
-			var contatoPage = '';
-			
-            $("#listacontatos li a").click(function(){
-            	var j = $(this).attr("href");
-            	var k = j.replace('#contatoItem','');
-            	if (data[k].lattes == null){
-            		var l = '#';
-            	} else {
-            		var l = data[k].lattes;
-            	}
 
-            	contatoPage += '<div data-role="page" id="contatoItem' + k + '">';
-            	contatoPage += '<div data-role="header" align="center" data-position="fixed"><a href="#contato" class="btn-normal">Voltar</a><img src="images/lg_feagri36.png"/></div>';
-				contatoPage += '<div role="main" class="ui-content">';
-				contatoPage += '<img src="http://www.feagri.unicamp.br/portal/'+data[k].foto+'">';
-				contatoPage += '<h3 class="ui-bar ui-bar-a">'+data[k].nome+'</h3>';
-				contatoPage += '<div class="ui-body">';
-          		contatoPage += '<p><strong>Grupo de Usuário: </strong>'+data[k].gruponame+'</p>';
-          		contatoPage += '<p><strong>Função: </strong>'+data[k].funcao+'</p>';
-          		contatoPage += '<p><strong>Email: </strong>';
-				contatoPage += '<a href="mailto:'+data[k].email+'">'+data[k].email+'</a></p>';
-          		contatoPage += '<p><strong>Telefone: </strong>';
-				contatoPage += '<a href="tel:'+data[k].telefone+'">'+data[k].telefone+'</a></p>';
-				contatoPage += '<p><strong>Orgão: </strong>'+data[k].orgao+'</p>';
-          		contatoPage += '<p><a href="'+l+'" target="_blank" >LATTES</a></p>';
-				contatoPage += '</div>';
-				contatoPage += '</div></div>';
-            	$("body").append(contatoPage);
-            	
+            $listacontatos.html(contatosHTML);
+
+            var contatoPage = '';
+
+            $("#listacontatos li a").click(function() {
+                var j = $(this).attr("href");
+                var k = j.replace('#contatoItem', '');
+                if (data[k].lattes == null) {
+                    var l = '#';
+                } else {
+                    var l = data[k].lattes;
+                }
+
+                contatoPage += '<div data-role="page" id="contatoItem' + k + '">';
+                contatoPage += '<div data-role="header" align="center" data-position="fixed"><a href="#contato" class="btn-normal">Voltar</a><img src="images/lg_feagri36.png"/></div>';
+                contatoPage += '<div role="main" class="ui-content">';
+                contatoPage += '<img src="http://www.feagri.unicamp.br/portal/' + data[k].foto + '">';
+                contatoPage += '<h3 class="ui-bar ui-bar-a">' + data[k].nome + '</h3>';
+                contatoPage += '<div class="ui-body">';          
+                contatoPage += '<p><strong>Grupo de Usuário: </strong>' + data[k].gruponame + '</p>';          
+                contatoPage += '<p><strong>Função: </strong>' + data[k].funcao + '</p>';          
+                contatoPage += '<p><strong>Email: </strong>';
+                contatoPage += '<a href="mailto:' + data[k].email + '">' + data[k].email + '</a></p>';          
+                contatoPage += '<p><strong>Telefone: </strong>';
+                contatoPage += '<a href="tel:' + data[k].telefone + '">' + data[k].telefone + '</a></p>';
+                contatoPage += '<p><strong>Orgão: </strong>' + data[k].orgao + '</p>';          
+                contatoPage += '<p><a href="' + l + '" target="_blank" >LATTES</a></p>';
+                contatoPage += '</div>';
+                contatoPage += '</div></div>';
+                $("body").append(contatoPage);
+
             });
 
-            
+
 
             /*
             for (var i = 0; i <data.length; i++) {
@@ -69,17 +69,10 @@ $(document).ready(function(){
           	} 
 			//+'<li><a href="'+data[i]['link']+'" target="_blank" >'+'LATTES'+'</a></li>'
 			*/
-		})
-		.fail(function(msg, textStatus, erro){
-			document.getElementById("listacontatos").innerHTML="não foi possível exibir os contatos...";
-            //alert(textStatus);
-			//alert(msg);
-			//alert(erro);
-            //console.log(erro);
-            //console.log(textStatus);
-            //console.log(msg);            
-		});
-		
-	
-});
+        })
+        .fail(function() {
+            $("#listacontatos").append('<p class="alerta">Não existem resultados a exibir!</p>');;
+        });
 
+
+});
