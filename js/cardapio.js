a427e77b910473122ecb2f4e6ebea4774f63f699
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2016-10-18 22:25:38 -0200

    Cardápio

---
+++ js/cardapio.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    var url = 'http://www.feagri.unicamp.br/portal/templates/simplesimon/includes/lercardapio.html';
+    //var url = 'js/cardapio.json';
+
+    var $cardapio = $(".cardapio");
+
+    $.getJSON(url)
+        .done(function(data) {
+            $cardapio.empty();
+
+            var cafe = '<div class="cafe"><h3>Café da Manhã</h3><p>'
+            cafe += data['Café da manhã'];
+            cafe += '</p></div>';
+
+            var almoco = '<div class="almoco"><h3>Almoço</h3><p>'
+            almoco += data['Almoço'].replace(/(?:\r\n|\r|\n)/g, '<br />');
+            almoco += '</p></div>';
+
+            var almocoVeg = '<div class="almocoVeg"><h3>Almoço Vegetariano</h3><p>'
+            almocoVeg += data['Almoço Vegetariano'].replace(/(?:\r\n|\r|\n)/g, '<br />');
+            almocoVeg += '</p></div>';
+
+            var jantar = '<div class="jantar"><h3>Jantar</h3><p>'
+            jantar += data['Jantar'].replace(/(?:\r\n|\r|\n)/g, '<br />');
+            jantar += '</p></div>';
+
+            var jantarVeg = '<div class="jantarVeg"><h3>Jantar Vegetariano</h3><p>'
+            jantarVeg += data['Jantar Vegetariano'].replace(/(?:\r\n|\r|\n)/g, '<br />');
+            jantarVeg += '</p></div>';
+
+            $cardapio.append(cafe + almoco + almocoVeg + jantar + jantarVeg);
+
+        })
+        .fail(function() {
+            $cardapio.append('<p class="alerta">Não existem cardápios disponíveis!</p>');;
+        }); // end fail
+
+}); // end document ready
